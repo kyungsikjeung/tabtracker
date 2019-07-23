13296bbe2aa379adfaf6c953c6bfb364a340da8a
kyungsikjeung <datamining7830@gmail.com>
2019-07-23 17:41:34 +0900

    app.js 에 require 로패키지 import 그리고 express()로 필요한 함수 사용

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
 console.log("hello");
+const express = require('express');
+// json 데이터 사용할떄
+const bodyParser = require('body-parser');
+// 현재 도메인에서 다른 도메인으로 리소스가 사용
+const cors = require('cors');
+// 
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status',(req,res)=>{
+    res.send({
+        message:'hello world!'
+    });
+})
+app.listen(process.env.PORT || 8081);
+
